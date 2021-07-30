baff36223242684d1dcbfc7d29dd97c6e0ebbc4c
Sid <sidchopp@gmail.com>
2021-07-30 15:14:29 -0600

    all modals show different content

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modals = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal')
 
 // When we click on the cross of modal or outside of the modal, we want the hidden class back
 
-const closeModal = function () {
-  modal.classList.add('hidden');
+const closeModal = function (element) {
+  element.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
-const showModal = function () {
+const showModal = function (element) {
   // To remove the hidden class. The classList property returns the class name of an element
   // remove method will remove the class( or classes by putting commas) mentioned in ().
   // Remember we don't use dot before class name hidden as dot is only for selectors and here we are not selecting anything(like we did with querySelector method) but here we are just passing name of the class
-  modal.classList.remove('hidden');
+  element.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
 
-// we use for loop for btnsShowModal as there are three modals, so this for loop iterates for each modal
+// we use for loop for modals, btnsShowModal and btnCloseModals as the same corresponding classes are used thrice, so this for loop iterates for each modal, showModal and closeModal
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', showModal)
+  btnsShowModal[i].addEventListener('click', function () {
+    showModal(modals[i])
+  });
+  btnCloseModal[i].addEventListener('click', function () {
+    closeModal(modals[i])
+  });
+  overlay.addEventListener('click', function () {
+    closeModal(modals[i])
+  });
+  document.addEventListener('keydown', function (e) {
+    // to see which key is pressed on the console
+    //console.log(e);
+    // to look for a specific proerty we use its name, like
+    //console.log(e.key);
+    // to close the modal with esc key, when the modal is already open
+    if (e.key === 'Escape' && !modals[i].classList.contains('hidden')) {
+      closeModal(modals[i]);
+    }
+  });
 };
 
-// Note we are not calling this function or we would have written closeModal().
-// The click event will call this function.
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-// for keyboard events( like we are using esc key to hide the modal)
-// keydown means "any" key we press
-document.addEventListener('keydown', function (e) {
-  // to see which key is pressed on the console
-  //console.log(e);
-  // to look for a specific proerty we use its name, like
-  //console.log(e.key);
-  // to close the modal with esc key, when the modal is already open
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
 
 
 
